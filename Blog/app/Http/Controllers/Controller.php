a1a35e9446aee09f2be785c56885c48ac86a58a4
code-with-mavia <maviaqaiser09@gmail.com>
2025-11-03 18:37:02 +0500

    updated controller file

--- Blog/app/Http/Controllers/Controller.php
+++ Blog/app/Http/Controllers/Controller.php
@@ -4,11 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    // public function getUser(){}
-    // public function getUserInfo() {}
-    // public function getPost(){}
-    // public function addNewPost($user_id, $title , $body) {}
-    // public function updatePost($id, $title , $body) {}
-    // public function deletePost($id) {}
 
 }
